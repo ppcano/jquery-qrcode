bc58f38154264d35bae3151dbab4e3a31a0ed8a3
Jerome Etienne <jerome.etienne@gmail.com>
2011-09-29 15:40:47 +0200

    Another deployement

--- src/jquery.qrcode.js
+++ src/jquery.qrcode.js
@@ -3,13 +3,20 @@
 		// if options is string, 
 		if( typeof options === 'string' ){
 			options	= { text: options };
-		}
+		};
+		
+		var supportCanvas	= function() {
+			var element	= document.createElement('canvas');
+			return !!(element.getContext && element.getContext('2d'));
+		};
 
 		// set default values
+		// typeNumber < 1 for automatic calculation
 		options	= $.extend( {}, {
+			render		: supportCanvas() ? "canvas" : "table",
 			width		: 256,
 			height		: 256,
-			typeNumber	: 4,
+			typeNumber	: -1,
 			correctLevel	: QRErrorCorrectLevel.H
 		}, options);
 
@@ -18,14 +25,17 @@
 			var qrcode	= new QRCode(options.typeNumber, options.correctLevel);
 			qrcode.addData(options.text);
 			qrcode.make();
+
 			// create canvas element
 			var canvas	= document.createElement('canvas');
 			canvas.width	= options.width;
 			canvas.height	= options.height;
 			var ctx		= canvas.getContext('2d');
+
 			// compute tileW/tileH based on options.width/options.height
 			var tileW	= options.width  / qrcode.getModuleCount();
 			var tileH	= options.height / qrcode.getModuleCount();
+
 			// draw in the canvas
 			for( var row = 0; row < qrcode.getModuleCount(); row++ ){
 				for( var col = 0; col < qrcode.getModuleCount(); col++ ){
@@ -37,9 +47,43 @@
 			return canvas;
 		}
 
+		// from Jon-Carlos Rivera (https://github.com/imbcmdth)
+		var createTable	= function(){
+			// create the qrcode itself
+			var qrcode	= new QRCode(options.typeNumber, options.correctLevel);
+			qrcode.addData(options.text);
+			qrcode.make();
+			
+			// create table element
+			var $table	= $('<table></table>')
+				.css("width", options.width+"px")
+				.css("height", options.height+"px")
+				.css("border", "0px")
+				.css("border-collapse", "collapse")
+				.css('background-color', "#ffffff");
+		  
+			// compute tileW/tileH
+			var tileW	= options.width  / qrcode.getModuleCount();
+			var tileH	= options.height / qrcode.getModuleCount();
+			// draw in the table
+			for(var row = 0; row < qrcode.getModuleCount(); row++ ){
+				var $row = $('<tr></tr>').css('height', tileH+"px").appendTo($table);
+				
+				for(var col = 0; col < qrcode.getModuleCount(); col++ ){
+					$('<td></td>')
+						.css('width', tileW+"px")
+						.css('background-color', qrcode.isDark(row, col) ? "#000000" : "#ffffff")
+						.appendTo($row);
+				}	
+			}
+			// return just built canvas
+			return $table;
+		}
+  
+
 		return this.each(function(){
-			var canvas	= createCanvas();
-			jQuery(canvas).appendTo(this);
+			var element	= options.render == "canvas" ? createCanvas() : createTable();
+			jQuery(element).appendTo(this);
 		});
 	};
 })( jQuery );
